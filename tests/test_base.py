25bf84156e0f20f13b6e1253e6635c9dafd0488d
Peter Hoffmann <ph@peter-hoffmann.com>
2017-03-05 16:12:27 +0100

    add required element to test

--- tests/test_base.py
+++ tests/test_base.py
@@ -4,7 +4,8 @@ def test_template():
     t = Template()
     d = t._asdict()
     expected = {'$schema': 'https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#', 
-                'contentVersion': '1.0.0.0'}
+                'contentVersion': '1.0.0.0',
+                'resources': []}
     assert d == expected
 
 def test_resource():
